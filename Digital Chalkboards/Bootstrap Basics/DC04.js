9bac5ae4d09a36847660a37afa385b608fb84c4c
sophiabgebreab <sophiagebreab@gmail.com>
2024-04-29 11:55:31 -0500

    did hw

---
+++ Digital Chalkboards/Bootstrap Basics/DC04.js
@@ -0,0 +1,27 @@
+function greetUser() {
+    const now = new Date();
+    const hour = now.getHours();
+    let greeting;
+
+    if (hour > 5 && hour < 11) {
+        greeting = "Good morning";
+    } else if (hour >= 12 && hour <18) {
+        greeting = "Good day";
+    } else {
+        greeting = "Good evening";
+    }
+    alert(greeting);
+}
+
+function changeButton() {
+    let button = document.getElementById("button2");
+    console.log("Before change:", button.textContent, button.className);
+
+    button.textContent = "Done";
+    button.className = "btn btn-success";
+
+    console.log("After change:", button.textContent, button.className);
+}
+
+document.getElementById("button1").addEventListener("click", greetUser);
+document.getElementById("button2").addEventListener("click", changeButton);
